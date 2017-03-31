babcae77a03fa2a702044554917705c5ff94cd87
Stan Chan <stanchanpsu@outlook.com>
2017-03-31 10:17:08 -0400

    Add source bashrc

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ git config --global user.email stanchanpsu@outlook.com
 git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=360000"
 git config --global push.default matching
+
+# Re-source .bashrc
+soure ~/.bashrc
